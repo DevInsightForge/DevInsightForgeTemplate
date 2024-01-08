2f64a073e94f0c9445fad265225779328abdf9f3
Mofajjal Rasul <mofajjalrasul@gmail.com>
2024-01-08 10:51:49 +0600

    refreshtoken: add validator for token refresh api

---
+++ template/DevInsightForge.Application/Authentication/Commands/RefreshAccessToken/RefreshAccessTokenCommandValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace DevInsightForge.Application.Authentication.Commands.RefreshAccessToken;
+
+public class RefreshAccessTokenCommandValidator : AbstractValidator<RefreshAccessTokenCommand>
+{
+    public RefreshAccessTokenCommandValidator()
+    {
+        RuleFor(command => command.RefreshToken)
+            .NotEmpty().WithMessage("Refresh Token is required.");
+    }
+}
